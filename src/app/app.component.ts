b45689c085aaeb5da7903afe82e47e53973fcea0
rajavasanthan <Rajavasanthan@gmail.com>
2020-05-20 12:47:49 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyFirstAngular';
+  price = 300;
+  isButtonDisabled = false;
+  url = "http://placehold.it/200x200"
+  userData = "Default value";
+
+  profile = {
+    name : "Person 1",
+    age : 30
+  }
+
+
+  constructor(){
+    setTimeout(() => {
+      this.title = "Changed Value"
+    },5000)
+  }
+
+  sendMessage(){
+    alert("The Message is Displayed")
+  }
+
+  sendMessage2(){
+    alert("Meesage from 2")
+  }
+
+  display(){
+    alert("Display Called")
+  }
+
+
 }
